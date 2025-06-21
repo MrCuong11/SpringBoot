63e4d7ea605868f34ed60dc399db509809188b76
Cuongcuch <nmanhcuong.cpf@gmail.com>
2025-06-21 09:40:04 +0700

    fix customer missing method

--- src/main/java/com/example/authdemo/repository/CustomerRepository.java
+++ src/main/java/com/example/authdemo/repository/CustomerRepository.java
@@ -194,4 +194,14 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
             "     AND c1.customer_id <> c2.customer_id " +
             ")", nativeQuery = true)
     int updateAddressForSameCityCustomers();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE customer SET address = CONCAT(address, 'updated') WHERE customer_id IN ( " +
+            "    SELECT DISTINCT c.customer_id " +
+            "    FROM customer c " +
+            "    JOIN rental r ON c.customer_id = r.customer_id " +
+            "    WHERE r.rental_date >= '2022-01-01' " +
+            ")", nativeQuery = true)
+    int updateCustomerAddresses();
 } 
